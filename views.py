54fa8e1c78c1303f913050397fb7c6686598abee
Praveen K <151647417+Praveenkalaiavanan@users.noreply.github.com>
2024-03-21 11:07:38 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import * 
+
+def home(request):
+    return render(request, "shop/index.html ")
+def register(request):
+    return render(request, "shop/register.html ")
+def collections(request):
+    catagory=Catagory.objects.filter(status=0)
+    return render(request, 'shop/collections.html', {'catagory': Catagory})
